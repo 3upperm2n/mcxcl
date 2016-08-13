cf251b5849606c0a8ce797a13d757cfc53ce262b
Leiming Yu <leimingyu830@gmail.com>
2016-08-13 16:18:55 -0400

    add device test

--- example/quicktest/titanx.sh
+++ example/quicktest/titanx.sh
@@ -4,4 +4,4 @@ if [ ! -e semi60x60x60.bin ]; then
   perl -pi -e 's/\x0/\x1/g' semi60x60x60.bin
 fi
 
-time ../../bin/mcxcl -t 8192 -T 64 -g 10 -n 1e7 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -k ../../src/mcx_core.cl -d 1 -G 01
+time ../../bin/mcxcl -t 24576 -T 64 -g 10 -n 1e7 -f qtest.inp -s qtest -r 1 -a 0 -b 0 -k ../../src/mcx_core.cl -d 0
